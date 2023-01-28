e181061a155fc1c9bac8d8088bcf92739aa5f923
donVolodya <volodymyr.novosad.knm.2020@lpnu.ua>
2023-01-28 14:17:44 +0200

    Initial Commit

---
+++ TwitterMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterMVVM
+//
+//  Created by Vova Novosad on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
